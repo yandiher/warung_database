af787bd31ac0d0a6d070b66f19067c27ec56d243
yandiher <iyaand333@gmail.com>
2023-10-05 13:10:06 +0700

    add primary key

--- warung fix.sql
+++ warung fix.sql
@@ -16,26 +16,32 @@ create table product_lookup (
 -- table for product out
 
 create table product_sales (
+    sales_id int auto_increment,
     barcode int,
     time_sale datetime not null,
+    primary key (sales_id),
 	foreign key (barcode) references product_lookup(barcode) on delete cascade
 );
 
 -- table for product in
 
 create table product_stock (
+    stock_id int auto_increment,
     barcode int,
     box int not null,
     product_in date not null,
     price_per_box int not null,
+    primary key (stock_id),
 	foreign key (barcode) references product_lookup(barcode) on delete cascade
 );
 
 -- table for missing products
 
 create table product_missing (
+    missing_id int auto_increment,
     barcode int,
     product_unit int not null,
     date_of_knowing date not null,
+    primary key (missing_id),
 	foreign key (barcode) references product_lookup(barcode) on delete cascade
 );
